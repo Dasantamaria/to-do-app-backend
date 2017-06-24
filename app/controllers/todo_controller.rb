761e55822701de80db1ae6947ae63886aa3e483c
Dahlia Santamaria <dasantamaria@cps.edu>
2017-06-24 18:19:53 +0000

    more todos

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -4,9 +4,21 @@ class TodoController < ApplicationController
     @math=4+4
     end
     def show 
-     @todo_description="walked the dogs"
-     @pomodoros=2
-     @id=params[:id]
+        @id=params[:id]
+        if @id == '1'
+            puts "hey there"
+            @todo_description="walked the dogs"
+            @pomodoros=2
+        elsif @id == '2'
+            puts "done"
+            @todo_description="go home"
+            @pomodoros=4
+            
+        else
+            puts "bye"
+            @todo_description="running"
+            @pomodoros=6
+        end
     end
 end 
 
