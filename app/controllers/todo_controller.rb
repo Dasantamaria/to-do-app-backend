9524df5a6b1e237b12639ea0a037ea116144a6ea
Dahlia Santamaria <dasantamaria@cps.edu>
2017-06-24 16:26:14 +0000

    the @ stuff

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -1,6 +1,11 @@
 
 class TodoController < ApplicationController 
-    def index
+    def index 
+    @math=4+4
+    end
+    def show 
+     @todo_description="walked the dogs"
+     @pomodoros=2
     end
 end 
 
