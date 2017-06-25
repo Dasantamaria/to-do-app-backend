f0c9cc5b5cb6a2407303560047784969a66f328c
Dahlia Santamaria <dasantamaria@cps.edu>
2017-06-25 16:03:08 +0000

    delete and edit button

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -10,10 +10,21 @@ class TodoController < ApplicationController
     end
     def create
          t = Todo.new
-  t.description = params['description']
-  t.pomodoro_estimate = params['pomodoro_estimate']
-  t.save
-  redirect_to "/todo/show/#{ t.id }"
-  end 
-end 
+         t.description = params['description']
+         t.pomodoro_estimate = params['pomodoro_estimate']
+         t.save
+        redirect_to "/todo/show/#{ t.id }"
+    end
+    def edit
+        @todo=Todo.find(params[:id])
+    end
+    def update
+        t = Todo.find(params[:id])
+         t.description = params['description']
+         t.pomodoro_estimate = params['pomodoro_estimate']
+         t.save
+        redirect_to "/todo/show/#{ t.id }"
+    end    
+end
+    
 
